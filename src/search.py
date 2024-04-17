afb9f9f485e46093e9904abf93366490a784ed5b
akalinin <am1kalinin@gmail.com>
2024-04-17 16:20:46 +0300

    calc n

--- src/search.py
+++ src/search.py
@@ -1,3 +1,5 @@
+import math
+
 import reader
 
 
@@ -5,11 +7,14 @@ def simple_search(polygon_data):
     for group in polygon_data['data']:
         group['elements'].sort(key=lambda d: d['contour'].area, reverse=True)
 
+        n = len(group['elements']) * group['percent'] / 100
+        print(math.ceil(n))
+
     return 0
 
 
 if __name__ == '__main__':
-    data = reader.read('tdc/input_2.json')
+    data = reader.read('tdc/input_4.json')
     simple_search(data)
 
     # a = [{'id': 1, 'value': 1.2},
